6a94721e5cb2132e83f069de87a488aa470a7ca7
YashaswiniYeramalli <69078326+YashaswiniYeramalli@users.noreply.github.com>
2021-07-01 00:31:50 +0530

    initial authors

---
+++ routes/books.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const router=express.Router()
+
+router.get('/',(req,res)=>{
+    res.render('books/index')
+})
+
+router.get('/new',(req,res)=>{
+    res.render('books/new')
+})
+
+module.exports=router
